6cf8122a97d0ff7aad472eacd7d039c4fdfb3420
Mordred <95609626+TTMordred@users.noreply.github.com>
2025-02-15 21:30:10 +0700

    setup

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      {/* Placeholder for content.  Consider adding a component here if one exists in the project that is not currently used. */}
+    </div>
+  )
+}
+
